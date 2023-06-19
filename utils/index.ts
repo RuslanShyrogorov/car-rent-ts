5862bec5c8115c8ee75ea8b14d807cd3c9b674da
Ruslan Shyrogorov <ruslanshirogorov@gmail.com>
2023-06-19 21:19:57 +0300

    initial repo

---
+++ utils/index.ts
@@ -0,0 +1,49 @@
+import { CarProps } from "@/types";
+
+export async function getAllCars() {
+  const headers = {
+    "X-RapidAPI-Key": "b708c41351mshbe56ddee01bd845p114380jsn823821018b2c",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
+
+  const response = await fetch(
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=forester",
+    { headers: headers }
+  );
+
+  const result = await response.json();
+
+  return result;
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; // base rental price per day
+
+  const mileageFactor = 0.1; // additional rate per mile driven
+
+  const ageFactor = 0.05; // additional rate per year of vehicle age
+
+  // Calculate additional rate on mileage and age
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  //total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL("https://cdn.imagin.studio/getimage");
+
+  const { make, year, model } = car;
+
+  url.searchParams.append("customer", "uasiton");
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  url.searchParams.append("angle", `${angle}`);
+
+  return `${url}`;
+};
